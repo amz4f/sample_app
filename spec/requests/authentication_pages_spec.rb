f79bba8c883e414679671b14c312994c68eb9d2f
Tony Zanella <tony.zanella@gmail.com>
2014-01-25 13:44:24 -0500

    decouple tests from implementation details

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,7 +18,7 @@ describe "Authentication" do
   	  before { click_button "Sign in" }
 
   	  it { should have_title('Sign in') }
-  	  it { should have_selector('div.alert.alert-error') }
+  	  it { should have_error_message('Invalid') }
   	end
 
     describe "after visiting another page" do
@@ -29,9 +29,7 @@ describe "Authentication" do
   	describe "with valid information" do
   	  let(:user) { FactoryGirl.create(:user) }
   	  before do
-  	  	fill_in "Email", with: user.email.upcase
-  	  	fill_in "Password", with: user.password
-  	  	click_button "Sign in"
+  	  	valid_signin(user)
   	  end
 
   	  it { should have_title(user.name) }
